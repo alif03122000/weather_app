2e2b5871463b29979c59cc5006d2d4cf61a17ef2
alif03122000 <u1904050@student.cuet.ac.bd>
2024-04-11 00:53:18 +0600

    Time is now available dynamic

---
+++ script.js
@@ -0,0 +1,16 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+
+
+setInterval(() =>{
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >=13 ? hour%12: hour;
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12? 'PM': 'AM';
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ ' '+ `<span id="am-pm">${ampm}</span>`
+}, 1000);
